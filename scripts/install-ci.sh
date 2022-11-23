c11842799cd4efa4bd3334f4b883cda0b6290fa8
Anthony Corletti <anthcor@gmail.com>
2022-11-23 00:25:25 -0500

    ci: install for linux

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -3,4 +3,4 @@
 pip install --upgrade pip maturin
 pip install --no-cache-dir '.[dev,test]'
 maturin build
-pip install --no-cache-dir target/wheels/*.whl
+pip install --no-cache-dir target/wheels/*linux_x86_64.whl
